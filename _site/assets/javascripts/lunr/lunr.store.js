acde9957c04bf3b4ea1f0bb9e6284e05c7dc45f7
Nelson Lai <wnellie@gmail.com>
2023-11-29 00:37:18 +0800

    Adding contents of _site

---
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Hi, it's nice to meet you!",
+        "excerpt":"Hello there ","categories": ["aboutus"],
+        "tags": [],
+        "url": "http://localhost:4000/aboutus/2023/11/29/Intro.html"
+      }]
